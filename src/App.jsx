a6fb6195cbb182a09227dd22f40dbb0255fabcb4
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2022-05-21 11:36:17 +0200

    Add suport for scss and bootsrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './App.scss';
+import Button from 'react-bootstrap/Button';
 
 function App() {
     return (
         <div>
             <h1>Hello world!</h1>
+            <Button variant="primary">Bootstrap</Button>
         </div>
     );
 }
