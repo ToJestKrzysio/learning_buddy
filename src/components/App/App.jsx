184566ed3c809d18ed7fef403661660aba22d128
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2022-05-21 15:12:09 +0200

    Add functionality to reach all endpoints via manual url input, add simple login and register endpoints

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from '../Header/Header';
 import Train from '../Train/Train';
 import Profile from '../Profile/Profile';
+import Login from '../Auth/Login/Login';
+import Register from '../Auth/Register/Register';
 
 function App() {
     return (
@@ -14,6 +16,8 @@ function App() {
                     <Route path="/" element={<h1>home</h1>} />
                     <Route path="/profile" element={<Profile />} />
                     <Route path="/train" element={<Train />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
                 </Routes>
             </BrowserRouter>
         </div>
