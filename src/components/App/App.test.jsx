24ac988e618f2fc7930a62af8b7f7d62abf2dc9b
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2022-05-21 14:17:41 +0200

    Fix jest configuration

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -5,5 +5,5 @@ import App from './App';
 
 test('It renderes Hello World in headline', () => {
     const { container } = render(<App />);
-    expect(container.querySelector('h1')).toContainHTML('Hello World!');
+    expect(container.querySelector('a')).toContainHTML('profile');
 });
